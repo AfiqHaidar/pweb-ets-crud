66715b8a3f0c5f383153f93c813c64da895ec447
Haidar <afiq.haidar12@gmail.com>
2023-10-26 19:11:44 +0700

    small adjustment

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,14 +13,14 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('person.dashboard')" :active="request()->routeIs('person.dashboard')">
-                        {{ __('Person') }}
+                        {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('person.show')" :active="request()->routeIs('person.show')">
-                        {{ __('Page') }}
+                        {{ __('Person') }}
                     </x-nav-link>
                 </div>
 
